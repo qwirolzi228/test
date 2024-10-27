edda7c4a5e037497cc78d688db395b3fb97d96b8
qwirolzy <lemat0509@gmail.com>
2024-10-27 14:36:28 +0300

    nothing

---
+++ test.py
@@ -0,0 +1,72 @@
+# a = input("Введите число")
+# if int(a) > 0:
+#     print('положительное')
+# elif int(a) < 0:
+#     print('отрицательное')
+# else:
+#     print('0')
+#
+# A=input('введите пароль')
+# b=input('повторите пароль ')
+# if A==b :
+#     print('пароль принят')
+# else:
+#     print('пароли не совподают')
+# a = int(input('введите число '))
+# if a%2==0:
+#     print('чётное')
+# else:
+#     print(('не чётное'))
+m = int(input('введите 1 число '))
+n = int(input('введите 2 число '))
+if m<n:
+    for i in range (m,n+1):
+        print(i)
+else:
+    for i in range (m,n-1,-1):
+        print(i)
+sdfghjkl;kjhh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
